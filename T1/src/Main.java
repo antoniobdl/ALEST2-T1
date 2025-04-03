68fd1033f21bfdc0769a7fd024fbee2fbd7dca09
antonioebdl <antonioebdl@dad.eng.br>
2025-04-03 20:31:10 -0300

    Testes search

--- T1/src/Main.java
+++ T1/src/Main.java
@@ -7,15 +7,20 @@ public class Main {
 
         //Gerar de maps e sets de diferentes tamanhos para usar nos testes
         Map<Integer, String> mapGrande = GeradorDicionarioConjunto.criarDicionario(100000);
+        Map<Integer, String> mapMedio = GeradorDicionarioConjunto.criarDicionario(10000);
         Set<Integer> setPequeno = GeradorDicionarioConjunto.criarConjunto(100);
 
         //Gerar search keys para simular busca, remoção, etc. Mesma idéia
-        Integer[] poucasKeys = GeradorDicionarioConjunto.gerarSearchKey(10, 100);
-        Integer[] muitasKeys = GeradorDicionarioConjunto.gerarSearchKey(10, 100000);
+        Integer[] poucasKeys = GeradorDicionarioConjunto.gerarSearchKey(1000, 1000);
+        Integer[] muitasKeys = GeradorDicionarioConjunto.gerarSearchKey(500000, 1000000);
 
         //Iniciar testes
+
+        System.out.println("Teste com conjunto pequeno:");
+        Testes.testarSearch(mapMedio, setPequeno, poucasKeys);
+
+        System.out.println("Teste com conjunto grande:");
         Testes.testarSearch(mapGrande, setPequeno, muitasKeys);
-        Testes.testarDelete(mapGrande, setPequeno, poucasKeys);
 
 
     }
--- T1/src/Testes.java
+++ T1/src/Testes.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.TreeMap;
-//Ir adicionando os imports necessários
+import java.util.TreeSet;
 
 public class Testes {
 
@@ -17,25 +17,100 @@ public class Testes {
         System.out.println("Tamanho do conjunto: " + set.size());
         System.out.println("Quantidade de chaves de busca: " + keys.length);
 
-        /*
-         Implementar testes de busca. ex:
+        // HashMap
+        long inicioHashMap = System.currentTimeMillis();
+        int achadosHashMap = 0;
+        for (Integer key : keys) {
+            if (map.containsKey(key)) {
+                achadosHashMap++;
+            }
+        }
+        long fimHashMap = System.currentTimeMillis();
+        System.out.println("\nHashMap: " + (fimHashMap - inicioHashMap) + "ms (" + achadosHashMap + " achados)");
 
-         long inicio = System.currentTimeMillis();
-         int qtd = 0;
-         for (Integer key : keys) {
-             if (dicionario.containsKey(key)) {
-                 qtd++;
-             }
-         }
-         long fim = System.currentTimeMillis();
-         System.out.println("Tempo de busca em dicionário: " + (fim - inicio) + " ms (" + qtd + " encontrados.)");
+        // HashSet
+        long inicioHashSet = System.currentTimeMillis();
+        int achadosHashSet = 0;
+        for (Integer key : keys) {
+            if (set.contains(key)) {
+                achadosHashSet++;
+            }
+        }
+        long fimHashSet = System.currentTimeMillis();
+        System.out.println("HashSet: " + (fimHashSet - inicioHashSet) + "ms (" + achadosHashSet + " achados)");
 
-         - Implementar testes de busca para set, arraylist, linkedlist, treeset, etc
+        // ArrayList - busca linear
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (Integer num : set) {
+            arrayList.add(num);
+        }
 
-         - Comparar resultados
+        long inicioArrayList = System.currentTimeMillis();
+        int achadosArrayList = 0;
+        for (Integer key : keys) {
+            if (arrayList.contains(key)) {
+                achadosArrayList++;
+            }
+        }
+        long fimArrayList = System.currentTimeMillis();
+        System.out.println("ArrayList: " + (fimArrayList - inicioArrayList) + "ms (" + achadosArrayList + " achados)");
 
-         - Print resultados
-        */
+        // LinkedList - busca linear
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (Integer num : set) {
+            linkedList.add(num);
+        }
+
+        long inicioLinkedList = System.currentTimeMillis();
+        int achadosLinkedList = 0;
+        for (Integer key : keys) {
+            if (linkedList.contains(key)) {
+                achadosLinkedList++;
+            }
+        }
+        long fimLinkedList = System.currentTimeMillis();
+        System.out.println("LinkedList: " + (fimLinkedList - inicioLinkedList) + "ms (" + achadosLinkedList + " achados)");
+
+        // TreeSet - busca log(n)
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (Integer num : set) {
+            treeSet.add(num);
+        }
+
+        long inicioTreeSet = System.currentTimeMillis();
+        int achadosTreeSet = 0;
+        for (Integer key : keys) {
+            if (treeSet.contains(key)) {
+                achadosTreeSet++;
+            }
+        }
+        long fimTreeSet = System.currentTimeMillis();
+        System.out.println("TreeSet: " + (fimTreeSet - inicioTreeSet) + "ms (" + achadosTreeSet + " achados)");
+
+        // TreeMap - busca log(n)
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            treeMap.put(entry.getKey(), entry.getValue());
+        }
+
+        long inicioTreeMap = System.currentTimeMillis();
+        int achadosTreeMap = 0;
+        for (Integer key : keys) {
+            if (treeMap.containsKey(key)) {
+                achadosTreeMap++;
+            }
+        }
+        long fimTreeMap = System.currentTimeMillis();
+        System.out.println("TreeMap: " + (fimTreeMap - inicioTreeMap) + "ms (" + achadosTreeMap + " achados)");
+
+        // Resumo
+        System.out.println("\nResumo busca:");
+        System.out.println("HashMap: \t" + (fimHashMap - inicioHashMap) + "ms");
+        System.out.println("HashSet: \t" + (fimHashSet - inicioHashSet) + "ms");
+        System.out.println("TreeMap: \t" + (fimTreeMap - inicioTreeMap) + "ms");
+        System.out.println("TreeSet: \t" + (fimTreeSet - inicioTreeSet) + "ms");
+        System.out.println("ArrayList: \t" + (fimArrayList - inicioArrayList) + "ms");
+        System.out.println("LinkedList: \t" + (fimLinkedList - inicioLinkedList) + "ms");
     }
 
     public static void testarInsert(int tamanho) {
